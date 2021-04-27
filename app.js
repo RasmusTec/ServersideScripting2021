ca2411dcffebda7d66a46080269d2d197d4bb6e1
RasmusTec <60211912+RasmusTec@users.noreply.github.com>
2021-04-27 11:14:01 +0200

    added static

--- app.js
+++ app.js
@@ -1,13 +1,19 @@
 const express = require('express');
-const morgan = require('morgan');
+const logger = require('morgan');
 const app = express();
 const port = process.env.PORT || 3000;
 
-const logger = require('morgan');
-app.use(logger('dev'));
+app.use(logger('dev',{
+    skip: req => !req.url.endsWith('.html') && req.url.indexOf('.') > -1
+}));
+
+app.set('view engine', 'ejs');
+app.set('views','./views');
+
+app.use(express.static('./public'));
 
 app.get('/',(req, res)=>{
-    res.send('Hello World!');
+    res.render('index');
 });
 
 app.listen(port,()=>{
